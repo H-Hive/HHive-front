a3b75f3ebccfc3d15eb3ed5805f81311be078cf5
de123456sdf <cdaeun95@gmail.com>
2024-01-30 14:47:17 +0900

    css 수정

--- src/components/PartyForm.vue
+++ src/components/PartyForm.vue
@@ -2,92 +2,65 @@
 // 파티에 대한 상세 정보(파티 조회) + 파티 가입/탈퇴 버튼(가입 여부에 따라 다름 눌렀을 시 정말 하시겠습니까 모달 창 뜸)
 
 <template>
-    <div v-if="partyData" class="party-card">
-
-      <div class="party-actions">
-        <button class="btn btn-outline-dark" v-if="isPartyHost" type="click" @click="openUpdatePartyModal">
-          수정하기
-        </button>
-        <button class="btn btn-outline-dark" v-if="isPartyHost" type="click" @click="openDeletePartyModal">
-          삭제하기
-        </button>
-        <button
-            v-if="isPartyHost"
-            @click="openSendNotToHIveModal"
-            class="btn btn-outline-dark"
-        >
-          알림 전송
-        </button>
-        <UpdatePartyModal
-            :id="partyData.id"
-            v-if="showUpdatePartyModal"
-            @modal-Closed="closeUpdatePartyModal"
-            @update-Success="handleUpdatePartyModalClosed"
-        />
-        <SendNotificationForm-modal
-            v-if="showSendNotModal"
-            :is-visible="showSendNotModal"
-            :id="partyData.id"
-            :type="groupType"
-            @closeModal="closeSendNotModal"
-            @send-Success="handleNotModalClosed"
-        />
-        <AlertModal
-            v-if="showAlertModal"
-            :is-visible="showAlertModal"
-            :message="modalMessage"
-            @closeModalAndRedirect="closeModalAndRedirect"
-        />
-        <DeletePartyModal
-            v-if="showDeletePartyModal"
-            :is-visible="showDeletePartyModal"
-            :id="partyData.id"
-            @delete-Success="handleDeletePartyModalClosed"
-            @closeModal="closeDeletePartyModal"
-        />
-      </div>
-
-      <div class="party-info">
-        <div class="party-title">
-          <h2>{{ partyData.title }}</h2>
-        </div>
-        <div class="host">
-          <p class="party-host" >방장</p>
-          <div class="party-host-data">
-            {{ partyData.username }}
-          </div>
-        </div>
-        <div class="datetime">
-          <p class="party-datetime">약속 날짜</p>
-          <div class="party-datetime-data">
-            {{ partyData.dateTime }}
-          </div>
-        </div>
-        <div class="content">
-          <p class="party-content">파티 내용</p>
-          <div class="content-data">
-            {{ partyData.content }}
-          </div>
-        </div>
-        <div class="members">
-          <p class="party-members">가입 멤버</p>
-          <div class="party-members-data">
-            {{ partyMember.join(', ') }}
-          </div>
-        </div>
-        <div class="join-btn" v-if="isHiveUser">
-          <div>
-            <JoinButton
-                :property="'Party'"
-                :id="partyData.id"
-                v-if="!isPartyUser"
-            />
-            <ResignButton :property="'Party'" :id="partyData.id" v-else />
-          </div>
-        </div>
-      </div>
-
+  <div>
+    <div v-if="partyData">
+      <h1>파티 이름: {{ partyData.title }}</h1>
+      <h1>파티 주최자: {{ partyData.username }}</h1>
+      <h1>파티 내용: {{ partyData.content }}</h1>
+      <h1>파티 일시: {{ partyData.dateTime }}</h1>
+      <h1>우리 멤버들: {{ partyMember }}</h1>
     </div>
+    <div class="join-btn" v-if="isHiveUser">
+      <div>
+        <JoinButton
+          :property="'Party'"
+          :id="partyData.id"
+          v-if="!isPartyUser"
+        />
+        <ResignButton :property="'Party'" :id="partyData.id" v-else />
+      </div>
+    </div>
+    <button v-if="isPartyHost" type="click" @click="openUpdatePartyModal">
+      수정하기
+    </button>
+    <button v-if="isPartyHost" type="click" @click="openDeletePartyModal">
+      삭제하기
+    </button>
+    <button
+      v-if="isPartyHost"
+      @click="openSendNotToHIveModal"
+      class="btn btn-outline-dark"
+    >
+      알림 전송
+    </button>
+    <UpdatePartyModal
+      :id="partyData.id"
+      v-if="showUpdatePartyModal"
+      @modal-Closed="closeUpdatePartyModal"
+      @update-Success="handleUpdatePartyModalClosed"
+    />
+    <SendNotificationForm-modal
+      v-if="showSendNotModal"
+      :is-visible="showSendNotModal"
+      :id="partyData.id"
+      :type="groupType"
+      @closeModal="closeSendNotModal"
+      @send-Success="handleNotModalClosed"
+    />
+    <AlertModal
+      v-if="showAlertModal"
+      :is-visible="showAlertModal"
+      :message="modalMessage"
+      @closeModalAndRedirect="closeModalAndRedirect"
+    />
+    <DeletePartyModal
+      v-if="showDeletePartyModal"
+      :is-visible="showDeletePartyModal"
+      :id="partyData.id"
+      @delete-Success="handleDeletePartyModalClosed"
+      @closeModal="closeDeletePartyModal"
+    />
+  </div>
 </template>
 
 <script>
@@ -223,104 +196,4 @@ export default {
 </script>
 
 <style scoped>
-
-.party-card {
-  width:60%;
-  margin-top: 30px;
-  padding: 30px;
-  border: 1px solid #aaaaaa;
-  border-radius: 8px;
-  margin-bottom: 150px;
-}
-
-.party-actions {
-  display: flex;
-  justify-content: flex-end;
-}
-
-.party-actions button {
-  margin-right: 10px;
-}
-
-.party-title {
-  border-bottom: 1px solid #aaaaaa;
-  padding: 10px;
-}
-
-.host {
-  display: flex;
-  align-items: center; /* 세로 방향에서 중앙 정렬 */
-  border-bottom: 1px solid #aaaaaa;
-}
-.party-host {
-  width: 10%;
-  display: flex;
-  align-items: center; /* 세로 방향에서 중앙 정렬 */
-  padding-top: 10px;
-  padding-left: 25px;
-}
-
-.datetime {
-  display: flex;
-  align-items: center; /* 세로 방향에서 중앙 정렬 */
-  border-bottom: 1px solid #aaaaaa;
-}
-.party-datetime {
-  padding-top: 10px;
-  padding-left: 25px;
-  width: 10%;
-}
-
-.party-datetime-data {
-}
-
-.content {
-  display: flex;
-  height: 400px;
-  border-bottom: 1px solid #aaaaaa;
-}
-
-.party-content {
-  display: flex;
-  align-items: center; /* 세로 방향에서 중앙 정렬 */
-  justify-content: center; /* 가로 방향에서 중앙 정렬 */
-  width: 30%;
-  border-right: 1px solid #aaaaaa;
-  margin-bottom: -1px;
-  max-height: 400px; /* 최대 높이 설정 */
-  overflow-y: auto; /* 세로 스크롤바가 필요할 때만 표시 */
-}
-
-.content-data {
-  padding: 20px;
-}
-
-.members {
-  display: flex;
-  align-items: center; /* 세로 방향에서 중앙 정렬 */
-  border-bottom: 1px solid #aaaaaa;
-  height: 100px;
-}
-
-.party-members {
-  display: flex;
-  align-items: center; /* 세로 방향에서 중앙 정렬 */
-  padding-top: 10px;
-  padding-left: 25px;
-  width: 10%;
-}
-
-.party-members-data {
-  width: 90%;
-  max-height: 100px; /* 최대 높이 설정 */
-  overflow-y: auto; /* 세로 스크롤바가 필요할 때만 표시 */
-}
-
-.join-btn {
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 25px;
-}
-
 </style>
-
--- src/components/UpdatePartyModal.vue
+++ src/components/UpdatePartyModal.vue
@@ -10,45 +10,27 @@
       <div>
         <h2>파티 수정</h2>
         <div class="line"></div>
-        <div class="content-top">
-          <div>
-            <label for="partyTitle">파티 이름:</label>
-            <input type="text" id="partyTitle" v-model="partyData.title" />
-          </div>
-          <div>
-            <label for="partyContent">파티 내용:</label>
-            <input type="text" v-model="partyData.content" />
-          </div>
-          <div>
-            <label for="partyYear">연도:</label>
-            <input type="text" v-model="partyData.year" />
-          </div>
-          <div>
-            <label for="partyMonth">월:</label>
-            <input type="text" v-model="partyData.month" />
-          </div>
-          <div>
-            <label for="partyDay">일:</label>
-            <input type="text" v-model="partyData.day" />
-          </div>
-          <div>
-            <label for="partyHours">시:</label>
-            <input type="text" v-model="partyData.hours" />
-          </div>
-          <div>
-            <label for="partyMinutes">분:</label>
-            <input type="text" v-model="partyData.minutes" />
-          </div>
-        </div>
+        <label for="partyTitle">파티 이름:</label>
+        <input type="text" id="partyTitle" v-model="partyData.title" />
+        <label for="partyContent">파티 내용:</label>
+        <input type="text" v-model="partyData.content" />
+        <label for="partyYear">연도:</label>
+        <input type="text" v-model="partyData.year" />
+        <label for="partyMonth">월:</label>
+        <input type="text" v-model="partyData.month" />
+        <label for="partyDay">일:</label>
+        <input type="text" v-model="partyData.day" />
+        <label for="partyHours">시:</label>
+        <input type="text" v-model="partyData.hours" />
+        <label for="partyMinutes">분:</label>
+        <input type="text" v-model="partyData.minutes" />
 
-        <div class="content-bottom">
-          <p :class="{ hidden: !errorMessage, red: errorMessage }">
-            {{ errorMessage }}
-          </p>
-          <button @click="updateParty" class="btn btn-outline-dark">
-            파티 수정
-          </button>
-        </div>
+        <p :class="{ hidden: !errorMessage, red: errorMessage }">
+          {{ errorMessage }}
+        </p>
+        <button @click="updateParty" class="btn btn-outline-dark">
+          파티 수정
+        </button>
       </div>
     </div>
   </div>
@@ -96,77 +78,48 @@ export default {
 </script>
   
   <style scoped>
-  .createparty-modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1;
-    width: 500px; /* 모달의 너비 조절 */
-    padding: 20px;
-    background-color: white;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  }
+.createparty-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  width: 300px; /* 모달의 너비 조절 */
+  padding: 20px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
 
-  .modal-content {
-    text-align: center;
-  }
+.modal-content {
+  text-align: center;
+}
 
-  .close {
-    color: #aaa;
-    font-size: 28px;
-    font-weight: bold;
-  }
+.close {
+  color: #aaa;
+  font-size: 28px;
+  font-weight: bold;
+}
 
-  .close:hover,
-  .close:focus {
-    color: black;
-    text-decoration: none;
-  }
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+}
 
-  .line {
-    border-bottom: 1px solid #000;
-    margin-bottom: 15px;
-  }
+.line {
+  border-bottom: 1px solid #000;
+  margin-bottom: 15px;
+}
 
-  .content-top {
-    display: flex; /* Flexbox 레이아웃 사용 */
-    justify-content: space-between; /* 요소들 사이에 공간을 균등하게 분배 */
-    align-items: center; /* 가로 방향에서 요소들을 중앙 정렬 */
-    padding: 20px;
-    height: 400px;
-    flex-direction: column;
-    margin-top: 10px;
-  }
-
-  .content-top > div {
-    display: flex; /* Flexbox 레이아웃 사용 */
-    justify-content: space-between; /* 요소들 사이에 공간을 균등하게 분배 */
-    align-items: center; /* 가로 방향에서 요소들을 중앙 정렬 */
-    width: 90%; /* div의 너비를 폼의 너비에 맞춤 */
-  }
-
-  .content-top input{
-    border: 1px solid black;
-    border-radius: 5px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-    width: 50%;
-    height: 30px;
-  }
-
-  .content-bottom {
-    width: 100%;
-    margin-top: 70px;
-    margin-bottom: 10px;
-  }
-
-  .hidden {
-    margin-top: 40px;
-  }
-  .red {
-    color: red;
-  }
+.btn {
+  margin-top: 20%;
+}
+.hidden {
+  margin-top: 40px;
+}
+.red {
+  color: red;
+}
 </style>
