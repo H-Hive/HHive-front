fb3c12f809a167bc96cf6523a627ca4a5b37cedb
mixedtape <54096846+mixedtape@users.noreply.github.com>
2024-02-03 18:07:51 +0900

    feat: 맵 기능 병합

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -22,6 +22,8 @@ export default {
       geocoder: null,
       clickedAddress: "",
       clickedLatLng: null,
+      lat: null,
+      lon:null,
     };
   },
   mounted() {
@@ -41,39 +43,50 @@ export default {
     },
     loadMap() {//사용자의 위치정보를 받아와서 맵에서 나오게하는 방식(사용자가 HTML5와 HTTPS사용시에만 가능(?)아마도. 오차좀 있음.
       const container = document.getElementById("map");
+      this.geocoder = new window.kakao.maps.services.Geocoder();
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
+            this.lat = position.coords.latitude;
+            this.lon = position.coords.longitude;
 
             const options = {
-              center: new window.kakao.maps.LatLng(lat, lon),
+              center: new window.kakao.maps.LatLng(this.lat, this.lon),
               level: 3,
             };
+
             this.map = new window.kakao.maps.Map(container, options);
-
             window.kakao.maps.event.addListener(this.map, 'click', this.handleMapClick);
-
-            this.loadMarker(lat, lon);
+            this.geocoder.addressSearch('경기 용인시 수지구 동천동 107-27', (result, status) => {
+              // 정상적으로 검색이 완료됐으면
+              if (status === window.kakao.maps.services.Status.OK) {
+                const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+                this.loadMarker(coords, '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>');
+              }
+            });
+            
+            this.loadCLickMarker(this.lat, this.lon);
           },
+
           () => {//사용자가 위치정보 동의를 거절했을경우 디폴트로 보여줄거.
             this.loadDefaultMap(container);
+            this.loadCLickMarker(this.lat, this.lon);
           }
         );
       } else {
         this.loadDefaultMap(container);
+        this.loadCLickMarker(this.lat, this.lon);
       }
+
     },
     handleMapClick(mouseEvent) {
 
       const latlng = mouseEvent.latLng;
-      this.geocoder = new window.kakao.maps.services.Geocoder();
-      this.loadMarker(latlng.getLat(), latlng.getLng());
+      this.loadCLickMarker(latlng.getLat(), latlng.getLng());
       this.clickedLatLng = { latitude: latlng.getLat(), longitude: latlng.getLng() };
       this.searchAddrFromCoords(latlng);
     },
-    loadMarker(lat, lng) {
+    loadCLickMarker(lat, lng) {
 
       if (!this.marker) {
         this.marker = new window.kakao.maps.Marker({
@@ -84,16 +97,29 @@ export default {
         this.marker.setPosition(new window.kakao.maps.LatLng(lat, lng));
       }
     },
+    loadMarker(coords, content) {
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      const marker = new window.kakao.maps.Marker({
+        map: this.map,
+        position: coords,
+      });
+
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      const infowindow = new window.kakao.maps.InfoWindow({
+        content: content,
+      });
+      infowindow.open(this.map, marker);
+    },
 
     searchAddrFromCoords(coords) {
 
       const geocoder = new window.kakao.maps.services.Geocoder();
-      
+
       geocoder.coord2Address(coords.getLng(), coords.getLat(), (result, status) => {
         if (status === window.kakao.maps.services.Status.OK) {
           this.clickedAddress = result[0].address.address_name;
-          this.$emit('getAddress-Success',this.clickedAddress);
-          
+          this.$emit('getAddress-Success', this.clickedAddress);
+
         }
       }
       );
